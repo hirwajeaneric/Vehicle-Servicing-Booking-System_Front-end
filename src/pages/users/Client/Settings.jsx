b9f5ce7b0543b25d5f76652cd43fa94ed92e6d18
mukarugwiro <mukarugwiroagnes@gmail.com>
2023-05-19 17:03:21 +0200

    Client user account complete

--- src/pages/users/Client/Settings.jsx
+++ src/pages/users/Client/Settings.jsx
@@ -10,6 +10,7 @@ export default function Settings() {
   const [user, setUser] = useState({});
 
   const [progress, setProgress] = useState({ value: '', disabled: false});
+  const [progress2, setProgress2] = useState({ value: '', disabled: false});
   const [open, setOpen] = useState(false);
   const [responseMessage, setResponseMessage] = useState({ message: '', severity: ''});
   const handleClose = (event, reason) => {
@@ -32,9 +33,8 @@ export default function Settings() {
   // Update user info
   const updateUser = (e) => {
     e.preventDefault();
-
-    setProgress({ value: 'UPDATING ...', disabled: true });
-    axios.put(`${Apis.bookingApis.update}${user._id}`, user)
+    setProgress({ value: 'Updating ...', disabled: true });
+    axios.put(`${Apis.userApis.updateUserAccount}${user._id}`, user)
     .then(response => {
       setTimeout(()=>{
         if (response.status === 200 || response.status === 201) {            
@@ -55,12 +55,12 @@ export default function Settings() {
 
   // Send password reset request
   const requestPasswordReset = () => {
-    setProgress({ value: 'SENDING REQUEST ...', disabled: true });
+    setProgress2({ value: 'SENDING REQUEST ...', disabled: true });
     axios.post(Apis.userApis.requestPasswordReset, {email: user.email})
     .then(response => {
       setTimeout(()=>{
         if (response.status === 200 || response.status === 201) {            
-          setProgress({ value: '', disabled: false });
+          setProgress2({ value: '', disabled: false });
           setResponseMessage({message: response.data.message, severity: 'success'});
           setOpen(true);  
         }
@@ -70,7 +70,7 @@ export default function Settings() {
       if (error.response && error.response.status >= 400 && error.response.status <= 500) {
         setResponseMessage({ message: error.response.data.msg, severity: 'error' });
         setOpen(true);
-        setProgress({ value: '', disabled: false });
+        setProgress2({ value: '', disabled: false });
       }
     });
   }
@@ -82,7 +82,7 @@ export default function Settings() {
         <meta name="description" content="User account settings."/> 
       </Helmet>
       <SectionOrPageContainer>
-        <h1 style={{ textAlign: 'left' }}>My bookings</h1>
+        <h1 style={{ textAlign: 'left' }}>My Account</h1>
         <FormContainer onSubmit={updateUser} style={{ flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
           <FirstPart>
             <DataColumn>
@@ -107,7 +107,8 @@ export default function Settings() {
           <ThirdPart>
             <DataColumn>
               <label htmlFor="resetPassword">Do you want to update your password? Click on the link bellow.</label>
-              <Button type='button' variant='text' size='small' color='primary' onClick={(e) => { e.preventDefault(); requestPasswordReset(); }}>Request Password Reset</Button>
+              {!progress2.disabled && <Button type='button' variant='text' size='small' color='primary' onClick={(e) => { e.preventDefault(); requestPasswordReset(); }}>Request Password Reset</Button>}
+              {progress2.disabled && <Button type='submit' variant='contained' size='small' color='primary' disabled>{progress2.value}</Button>}
             </DataColumn>
           </ThirdPart>
         </FormContainer>
